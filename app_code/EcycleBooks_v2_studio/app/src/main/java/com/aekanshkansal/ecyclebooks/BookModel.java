d348ddc34e2e26fec4d133d6104ac26a32f2bf98
aekanshkansal1 <aekanshkansal1@gmail.com>
2017-05-09 10:38:55 +0530

    app code sync

---
+++ app_code/EcycleBooks_v2_studio/app/src/main/java/com/aekanshkansal/ecyclebooks/BookModel.java
@@ -0,0 +1,27 @@
+package com.aekanshkansal.ecyclebooks;
+
+public class BookModel {
+
+	public String userId;
+	public String bookName;
+	public String bookAuthor;
+	public String bookPublication;
+	
+	public String getBookName()
+	{
+		return this.bookName;
+	}
+	public String getUserId()
+	{
+		return this.userId;
+	}
+	public String getBookAuthor()
+	{
+		return this.bookAuthor;
+	}
+	public String getBookPublication()
+	{
+		return this.bookPublication;
+	}
+
+}
